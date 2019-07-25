7bfba50c899c4de287f81dd1cb65d49b9e81c106
raulej18 <50030217+raulej18@users.noreply.github.com>
2019-07-25 20:37:12 -0300

    Add more Assets to the Scene

---
+++ Challenge/Assets/Scripts/Player/Rotation.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		FaceMouse ();
+	}
+
+	private void FaceMouse(){
+		Vector3 mousePosition = Input.mousePosition;
+		mousePosition = Camera.main.ScreenToWorldPoint (mousePosition);
+
+		Vector2 direction = new Vector2 (mousePosition.x-transform.position.x,mousePosition.y-transform.position.y);
+
+		transform.up = direction;
+
+	}
+}
