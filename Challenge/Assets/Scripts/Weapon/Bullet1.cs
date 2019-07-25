373fb602e975d2679b46e5d2065425628940ffe9
raulej18 <50030217+raulej18@users.noreply.github.com>
2019-07-25 20:55:03 -0300

    Add Weapon

---
+++ Challenge/Assets/Scripts/Weapon/Bullet1.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullet1 : MonoBehaviour {
+	private int speed=15;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.up*speed*Time.deltaTime);
+		Destroy (gameObject, 1.5f);
+	}
+}
