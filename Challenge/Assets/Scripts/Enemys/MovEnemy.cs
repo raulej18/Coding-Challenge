0378f5fd4ac3619a67eed5156f6e4974cdf28215
raulej18 <50030217+raulej18@users.noreply.github.com>
2019-07-26 18:23:16 -0300

    AddEnemys

---
+++ Challenge/Assets/Scripts/Enemys/MovEnemy.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovEnemy : MonoBehaviour {
+	private float speed=3.5f;
+
+	private Vector2 direction;
+	private bool directionUpdate;
+	private int changeTime;
+	private float time;
+
+	// Use this for initialization
+	void Start () {
+
+		changeTime = Random.Range (3,15);	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!directionUpdate) {
+			GenerateDirection();
+		} 
+		else {
+			transform.Translate(Vector3.up*speed*Time.deltaTime);
+			if(time<changeTime){
+				time+=Time.deltaTime;
+
+			}
+			else{
+				time=0;
+				directionUpdate=false;
+			}
+		}
+
+	}
+	private void GenerateDirection(){
+		direction = new Vector2 (Random.Range (1, 49), Random.Range (1, 49));
+		Vector2 aux = direction;
+		aux= new Vector2 (aux.x-transform.position.x,aux.y-transform.position.y);
+		transform.up = aux;
+		directionUpdate = true;
+
+	}
+}
--- Challenge/Assets/Scripts/Generators/GTreeAndBuilding.cs
+++ Challenge/Assets/Scripts/Generators/GTreeAndBuilding.cs
@@ -5,9 +5,21 @@ public class GTreeAndBuilding : MonoBehaviour {
 	public GameObject[] generates;
 	public int[] mount;
 	public int total;
+
+	private int i;
+
+	private int[] posX;
+	private int[] posY;
+	private int countX;
+	private int countY;
+
 	// Use this for initialization
 	void Start () {
+		posX = posY= new int[48];
+		countX = countY = 0;
+		i = 0;
 		GenerateObjects();
+
 	}
 	
 	// Update is called once per frame
@@ -16,7 +28,7 @@ public class GTreeAndBuilding : MonoBehaviour {
 	}
 
 	private void GenerateObjects(){
-		for (int i=0; i<total; i++) {
+		while(i<total) {
 			if(i<mount[0]){
 				Generate (generates[0]);
 			}
@@ -27,17 +39,42 @@ public class GTreeAndBuilding : MonoBehaviour {
 				Generate (generates[2]);
 			}
 			else if(i<mount[3]){
-				Generate (generates[3]);
+				Generate(generates[3]);
 			}
 			else {
 				Generate (generates[4]);
 			}
+			i++;
 		}
 	}
 	private void Generate(GameObject b){
 		int x = Random.Range (1,49);
 		int y = Random.Range (1, 49);
-		GameObject baseP=Instantiate(b,new Vector3(x*3,y*3,-2),Quaternion.identity)as GameObject;
-		baseP.transform.parent = transform;
+		if (RepeatedPosition (x, y)) {
+			Generate (b);
+		} 
+		else {
+			GameObject baseP=Instantiate(b,new Vector3(x*3,y*3,-2),Quaternion.identity)as GameObject;
+			baseP.transform.parent = transform;
+			posX[countX]=x;
+			posY[countY]=y;
+		}
+
+
 	}
+	private bool RepeatedPosition(int x, int y){
+		for (int i=0; i<posX.Length; i++) {
+			if(posX[i]!=0){
+				if(posX[i]==x && posY[i]==y){
+					return true;
+				}
+
+			}
+			else{
+				return false;
+			}
+		}
+		return false;
+	}
+
 }
